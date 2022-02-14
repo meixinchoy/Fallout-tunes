6c5c54142c16d5d6e2be170608d9ee7b1f891927
meixin <meixinchoy@gmail.com>
2022-02-14 18:30:38 +0800

    done with title position

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
+import { Image, Switch } from 'antd';
+import { useState } from 'react';
 
 function App() {
+  const [DCR, setDCR] = useState(true);
+
   return (
     <div className="App">
-      <header>
-        <title>Wasteland Radio Station</title>
-        <meta name="description" content="Wasteland Radio Station" />
-        <link rel="icon" href="/icon.ico" />
-      </header>
+      <span class="titleSpan"><img src="title.png" alt="fallout-font" border="0" /></span>
+
       {/* classical- https://open.spotify.com/embed/playlist/24hFTiVobvxt554Oh9bm6g?utm_source=generator */}
       {/* <h1>Wasteland Radio Station</h1> */}
       <span class="titleSpan"><img src="https://fontmeme.com/permalink/220202/7a3a3932b0e1a6d8535281496a1d5afa.png" alt="fallout-font" border="0" class="titletext" /></span>
       <iframe title="song-playlist" class="playlist" src="https://open.spotify.com/embed/playlist/11EqHijOXQDZ1pfKSCu0h7?utm_source=generator" frameBorder="" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+
     </div>
+      
+    </div >
   );
 }
 
